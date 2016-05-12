dbdb4c4ad16ddac242cb334fe496a9240766fb17
김영 <young891221@gmail.com>
2016-05-12 17:07:20 +0900

    구글 로그인 추가 수정

--- src/main/java/net/mwav/member/contrloller/AuthController.java
+++ src/main/java/net/mwav/member/contrloller/AuthController.java
@@ -1,12 +1,13 @@
 package net.mwav.member.contrloller;
 
 import java.io.IOException;
-import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
 import net.mwav.member.auth.AuthUser;
+import net.mwav.member.auth.GoogleAuthHelper;
 import net.mwav.member.auth.GoogleAuthentication;
+import net.mwav.member.auth.GoogleUser;
 import net.mwav.member.auth.UserInfo;
 import net.mwav.member.service.AuthService;
 
@@ -27,6 +28,7 @@ public class AuthController {
 	private static final Logger logger = Logger.getLogger(AuthController.class);
 	
 	@Autowired private AuthService authService;
+	@Autowired private GoogleAuthHelper googleAuthHelper;
 	
 
 	@RequestMapping(value = "/accessDenied", method = RequestMethod.GET)
@@ -34,6 +36,72 @@ public class AuthController {
 		model.addAttribute("message", "접근 권한이 없음");
 		return "accessDenied";
 	}
+	
+	/*@RequestMapping(value = "/auth/callback", method = RequestMethod.GET)
+	public String authCallback(@RequestParam(value = "code", required = false) String authCode, 
+								@RequestParam("state") String callbackUrl,Model model) {	
+		String rdUrl = callbackUrl;
+
+		
+		GoogleUser googleUserInfo=null;
+		try {
+			if(authCode == null) 
+				new RuntimeException("Auth Code can't null!!");
+			
+			googleUserInfo = googleAuthHelper.getUserInfoJson(callbackUrl, authCode);
+			
+			if(googleUserInfo==null) 
+				new RuntimeException("GoogleUserInfo can`t null!");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return getAuthResult(callbackUrl, null,false);
+		}
+
+
+		AuthUser user = authService.getAuthUser(googleUserInfo);
+		if(user.getState() == State.UNUSED)
+			return getAuthResult(callbackUrl, null, false);
+		
+		return getAuthResult(callbackUrl, user, true);
+	}
+
+
+	private String getAuthResult(String url, AuthUser user, boolean result) {
+		StringBuilder builder = new StringBuilder().append("redirect:").append(url);
+		
+		if(url.contains("?"))
+			builder.append("&");
+		else 
+			builder.append("?");
+		if(result) 
+			builder.append("token=").append(tokenService.getTimeToken(user));
+		else 
+			builder.append("token=").append("null");
+		
+		builder.append("&result=").append(result);
+		
+		return builder.toString();
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@ResponseBody
+	public UserInfo getUserInfo(@RequestParam("token") String token, 
+								@RequestParam(value = "hash", required = false, defaultValue="") String hash, Model model) {
+		UserInfo user;
+		try{
+			int userIdx = tokenService.getUserInfo(token);
+			
+			if(userIdx == 0)
+				throw new RuntimeException("잘못된 token 입력");
+			
+    		user = (hash.length() == 0) ? authService.getUserInfo(userIdx): authService.getUserInfo(userIdx, hash);
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    		user = UserInfo.createFailUserInfo(e.getMessage());
+    	}
+		return user;
+	}*/
 
 	/**
 	 * 로그인이 성공했을 때, callback으로 호출되는 주소 <br />
@@ -44,12 +112,31 @@ public class AuthController {
 	 * @return
 	 * @throws IOException
 	 */
-	@RequestMapping(value = "/authentication", method = RequestMethod.GET)
-	public String authentication(HttpServletRequest request, Model model, @RequestParam(value = "token", required = false) String authCode, 
-						@RequestParam(value = "result", required = false) boolean result) throws IOException {
-		logger.info("authentication!!");
+	@RequestMapping(value = "/authentication.do", method = RequestMethod.GET)
+	public String authentication(HttpServletRequest request, Model model, @RequestParam(value = "code", required = false) String authCode, 
+						@RequestParam(value = "state", required = false) String callbackUrl) throws IOException {
+		System.out.println("authentication!!");
+		System.out.println("authCode = "+authCode);
+		System.out.println("state = "+callbackUrl);
+		System.out.println("request = "+request);
+		System.out.println("model = "+model);
 		if(authCode == null) {
-			return "login";
+			return "login.do";
+		}
+		
+		GoogleUser googleUser=null;
+		try {
+			if(authCode == null) 
+				new RuntimeException("Auth Code can't null!!");
+			
+			googleUser = googleAuthHelper.getUserInfoJson(callbackUrl, authCode);
+			
+			if(googleUser==null) 
+				new RuntimeException("GoogleUserInfo can`t null!");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "accessdenied";
 		}
 		
 		UserInfo googleUserInfo = authService.readUserInfo(authCode);
@@ -61,18 +148,10 @@ public class AuthController {
 			SecurityContextHolder.getContext().setAuthentication(new GoogleAuthentication(authInfo));
 		}
 		
-		request.getSession().setAttribute("currentUserUid", googleUserInfo.getName());
+		request.getSession().setAttribute("currentUser", googleUserInfo.getName());
 		
 		return "redirect:/";
 	}
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
-		return "redirect:/login";
-	}
 	
 }
--- src/main/java/net/mwav/member/service/AuthService.java
+++ src/main/java/net/mwav/member/service/AuthService.java
@@ -5,4 +5,8 @@ import net.mwav.member.auth.UserInfo;
 public interface AuthService {
 	
 	public UserInfo readUserInfo(String token);
+	
+	//public UserInfo getUserInfo(int idx) throws RuntimeException;
+	
+	//public UserInfo getUserInfo(int idx, String hash) throws RuntimeException;
 }
