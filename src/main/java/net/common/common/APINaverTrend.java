7d5fb84c7afb76eeb224c443aadbf35ab4cc5522
김주성 <jusung.kim@mwav.net>
2018-05-16 23:44:43 +0900

    - HighChart - 네이버 데이터랩 API 연동을 위하여 자바 소스 추가 (네이버 API 키값 포함)

---
+++ src/main/java/net/common/common/APINaverTrend.java
@@ -0,0 +1,52 @@
+package net.common.common;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class APINaverTrend {
+
+    public static void main(String[] args) {
+        String clientId = "IgsxtB0uBqToYHbY1TEK";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "Md4yzO3biF";//애플리케이션 클라이언트 시크릿값";
+
+        try {
+            String apiURL = "https://openapi.naver.com/v1/datalab/search";
+            String body = "{\"startDate\":\"2017-01-01\",\"endDate\":\"2017-12-30\",\"timeUnit\":\"month\",\"keywordGroups\":[{\"groupName\":\"한글\",\"keywords\":[\"한글\",\"korean\"]},{\"groupName\":\"영어\",\"keywords\":[\"영어\",\"english\"]}],\"device\":\"pc\",\"ages\":[\"1\",\"2\"],\"gender\":\"f\"}";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            con.setRequestProperty("Content-Type", "application/json");
+
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.write(body.getBytes());
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
