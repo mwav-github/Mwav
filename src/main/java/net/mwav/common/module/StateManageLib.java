5d5d074449ec722fce04bf2628a5b0b1cdf3c03e
tony950620 <tony950620@gmail.com>
2019-04-26 20:36:51 +0900

    20190426

--- src/main/java/net/mwav/common/module/StateManageLib.java
+++ src/main/java/net/mwav/common/module/StateManageLib.java
@@ -8,35 +8,85 @@ import java.util.Map;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ *@class name : StateManageLib.java
+ *@description : 
+ *@author : (정) 공태현
+            (부)
+ *@since : 2019. 3. 30.
+ *@version : v1.0
+ *@see :    
+ * @history :
+   ----------------------------------------
+   * Modification Information(개정이력)
+   ----------------------------------------
+   수정일     수정자        수정내용
+   --------    --------    ----------------
+   2019. 3. 30.     John   최초작성 
+ */
 public class StateManageLib {
-
-	/* 싱글톤 구현 
-	========각 메서드에 대한 설명과 호출하는 부분 명시========
-	설명 : StateManageLib 은 하나의 클래스에 대해서 단 하나의 인스턴스만을 생성할 수 있도록 제한 싱글톤형태로 생성한다.
-	              (동기화 적용-하나의 인스턴스에 참조에 대해서 동시접근 불가)  
-	호출하는 부분에서 사용시 : StateManageLib stateManageLib = StateManageLib.getInstance();
-	*/	
-	//public StateManageLib() {}	
 	
 	private static StateManageLib stateManageLib;
 	
 	// 쿠키를 <쿠키이름, cookie 객체> 쌍으로 저장하는 맵
 	private Map cookieMap = new java.util.HashMap();
 	
+	/** 
+	 * @method name : getInstance
+	 * @author : (정) 공태현
+	                 (부)
+	 * @since  : 2019. 3. 30.
+	 * @version : v1.0
+	 * @see :
+	 * @description : StateManageLib 은 하나의 클래스에 대해서 단 하나의 인스턴스만을 생성할 수 있도록 제한 싱글톤형태로 생성한다. 
+	 * 				  (동기화 적용-하나의 인스턴스에 참조에 대해서 동시접근 불가)	 
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	   수정일     수정자        수정내용
+	   --------    --------    ----------------
+	   2019. 3. 30.     John     
+	 * @param :
+	 * @return : 첫 호출이면 생성 또는 기존에 생성했던 인스턴스 반환
+	 * @throws : 
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * StateManageLib stateManageLib = StateManageLib.getInstance();
+	 * } 
+	 </pre>
+	*/
 	public static synchronized StateManageLib getInstance(){
 		if(stateManageLib ==null){
 			stateManageLib = new StateManageLib();
 		}
 		return stateManageLib;   
 	}	
-
 	
-	/**
-	 * 유저의 요청에 담긴 쿠키를 가져와 배열로 저장한다.
-	 * 
-	 * @param request
-	 * @throws IOException
-	 */	
+	/** 
+	 * @method name : AddCookieBox
+	 * @author : (정) 공태현
+	                 (부)
+	 * @since  : 2019. 3. 30.
+	 * @version : v1.0
+	 * @see :
+	 * @description : 유저의 요청에 담긴 쿠키를 가져와 StateManageLib 인스턴스의 배열로 저장한다.
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	   수정일     수정자        수정내용
+	   --------    --------    ----------------
+	   2019. 3. 30.     John     
+	 * @param : request
+	 * @return :
+	 * @throws : IOException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * stateManageLib.AddCookieBox(request);
+	 * } 
+	 </pre>
+	*/
 	public void AddCookieBox(HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
 		if (cookies != null) {
@@ -46,29 +96,59 @@ public class StateManageLib {
 		}
 	}	
 	
-	/**
-	 * 쿠키객체를 생성해서 리턴
-	 * 
-	 * @param name
-	 * @param value
-	 * @return cookie
-	 * @throws IOException
-	 */
+	/** 
+	 * @method name : createCookie
+	 * @author : (정) 공태현
+	                 (부)
+	 * @since  : 2019. 3. 30.
+	 * @version : v1.0
+	 * @see :
+	 * @description : 쿠키객체를 생성해서 리턴
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	   수정일     수정자        수정내용
+	   --------    --------    ----------------
+	   2019. 3. 30.     John     
+	 * @param : name, value
+	 * @return : cookie
+	 * @throws : IOException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * stateManageLib.createCookie("name","value");
+	 * } 
+	 </pre>
+	*/
 	public static Cookie createCookie(String name, String value)
 			throws IOException {
 		return new Cookie(name, URLEncoder.encode(value, "utf-8"));
 	}
 	
-	/**
-	 * 쿠키객체를 생성해서 리턴
-	 * 
-	 * @param name
-	 * @param value
-	 * @param path
-	 * @param maxAge  (일,시간,분,초 //  365*24*60*60 : 1년 // 20*60 : 20분)
-	 * @return cookie
-	 * @throws IOException
-	 */
+	/** 
+	 * @method name : createCookie
+	 * @author : (정) 공태현
+	                 (부)
+	 * @since  : 2019. 3. 30.
+	 * @version : v1.0
+	 * @see :
+	 * @description : 쿠키객체를 생성해서 쿠키의 유효한 디렉토리와 유효기간의 값 셋팅해주고 리턴
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	   수정일     수정자        수정내용
+	   --------    --------    ----------------
+	   2019. 3. 30.     John     
+	 * @param : name, value, path, maxAge  (일,시간,분,초 //  365*24*60*60 : 1년 // 20*60 : 20분)
+	 * @return : cookie
+	 * @throws : IOException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * stateManageLib.createCookie("name","value","/","60 * 60 * 24 * 14");
+	 * } 
+	 </pre>
+	*/
 	public static Cookie createCookie(String name, String value, String path,
 			int maxAge) throws IOException {
 		Cookie cookie = new Cookie(name, URLEncoder.encode(value, "utf-8"));
@@ -77,18 +157,30 @@ public class StateManageLib {
 		return cookie;
 	}
 	
-	/**
-	 * 쿠키객체를 생성해서 리턴
-	 * 
-	 * @param name
-	 * @param value
-	 * @param domain
-	 * @param path
-	 * @param maxAge
-	 * @return cookie
-	 * @throws IOException
-	 */
-	
+	/** 
+	 * @method name : createCookie
+	 * @author : (정) 공태현
+	                 (부)
+	 * @since  : 2019. 3. 30.
+	 * @version : v1.0
+	 * @see :
+	 * @description : 쿠키객체를 생성해서 유효한 도메인과 쿠키의 유효한 디렉토리, 유효기간의 값 셋팅해주고 리턴
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	   수정일     수정자        수정내용
+	   --------    --------    ----------------
+	   2019. 3. 30.     John     
+	 * @param : name, value, domain, path, maxAge  (일,시간,분,초 //  365*24*60*60 : 1년 // 20*60 : 20분)
+	 * @return : cookie
+	 * @throws : IOException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * stateManageLib.createCookie("name","value", "mwav.com", "/","60 * 60 * 24 * 14");
+	 * } 
+	 </pre>
+	*/	
 	public static Cookie createCookie(String name, String value, String domain,
 			String path, int maxAge) throws IOException {
 		Cookie cookie = new Cookie(name, URLEncoder.encode(value, "utf-8"));
@@ -98,23 +190,58 @@ public class StateManageLib {
 		return cookie;
 	}
 	
-	/**
-	 * 쿠키를 가져온다
-	 * 
-	 * @param name
-	 * @return cookie
-	 */
+	/** 
+	 * @method name : getCookie
+	 * @author : (정) 공태현
+	                 (부)
+	 * @since  : 2019. 3. 30.
+	 * @version : v1.0
+	 * @see :
+	 * @description : 쿠키를 가져온다
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	   수정일     수정자        수정내용
+	   --------    --------    ----------------
+	   2019. 3. 30.     John     
+	 * @param : name
+	 * @return : 인스턴스에서 name의 쿠키객체를 가져온다.
+	 * @throws : 
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * stateManageLib.getCookie("name");
+	 * } 
+	 </pre>
+	*/
 	public Cookie getCookie(String name) {
 		return (Cookie) cookieMap.get(name);
 	}	
 	
-	/**
-	 * 쿠키의 값을 가져온다
-	 * 
-	 * @param name
-	 * @return value
-	 * @throws IOException
-	 */
+	/** 
+	 * @method name : getValue
+	 * @author : (정) 공태현
+	                 (부)
+	 * @since  : 2019. 3. 30.
+	 * @version : v1.0
+	 * @see :
+	 * @description : 인스턴스에서 name의 쿠키객체의 value만 디코딩 후 리턴
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	   수정일     수정자        수정내용
+	   --------    --------    ----------------
+	   2019. 3. 30.     John     
+	 * @param : name
+	 * @return : 쿠키객체의 value만 리턴
+	 * @throws : IOException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * stateManageLib.getValue("name");
+	 * } 
+	 </pre>
+	*/
 	public String getValue(String name) throws IOException {
 		Cookie cookie = (Cookie) cookieMap.get(name);
 		if (cookie == null)
@@ -122,14 +249,30 @@ public class StateManageLib {
 		return URLDecoder.decode(cookie.getValue(), "utf-8");
 	}
 	
-
-	/**
-	 * 쿠키 삭제
-	 * 
-	 * @param cookieKey
-	 * @return
-	 * @throws IOException 
-	 */
+	/** 
+	 * @method name : deleteCookie
+	 * @author : (정) 공태현
+	                 (부)
+	 * @since  : 2019. 3. 30.
+	 * @version : v1.0
+	 * @see :
+	 * @description : 인스턴스의 cookieKey을 제거
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	   수정일     수정자        수정내용
+	   --------    --------    ----------------
+	   2019. 3. 30.     John     
+	 * @param : cookieKey
+	 * @return :
+	 * @throws : IOException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * stateManageLib.deleteCookie("cookieKey");
+	 * } 
+	 </pre>
+	*/
 	public Cookie deleteCookie(String cookieKey) throws IOException {
 		Cookie cookie = null;
 		if (getCookie(cookieKey) != null) {
