9101b8e0e68ced9fae42460667f3d217c55bbcaf
tony <tony950620@naver.com>
2019-07-22 11:13:13 +0900

    20190722 회원가입 이메일 발신, 질문메일 발신 메소드 개발 및 코드 간소화

--- src/main/java/net/mwav/common/module/MailLib.java
+++ src/main/java/net/mwav/common/module/MailLib.java
@@ -1,167 +1,3 @@
-<<<<<<< HEAD
-package net.mwav.common.module;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import javax.mail.Authenticator;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-
-import org.springframework.ui.velocity.VelocityEngineUtils;
-import org.springframework.web.servlet.view.velocity.VelocityConfig;
-import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
-
-/**
- *@class name : MailLib.java
- *@description : It's MailLib
- *				 This lib has the ability to send or verify email. 
- *				 MailLib made Singleton Design Patterm. 
- *				 To use MailLib, Create getInstence() method. 
- *
- *				 Default Properties Settings
- *				  - Host : smtp.gmail.com
- *				  - Port : 587
- *@author : (정) 공태현
-            (부)
- *@since : 2019. 7. 17.
- *@version : v1.0
- *@see
- * @history :
-   ----------------------------------------
-   * Modification Information(개정이력)
-   ----------------------------------------
-           수정일                  수정자                         수정내용
-   --------    --------    ----------------
-   2019. 7. 17.     John    
- */
-public class MailLib {
-
-	private Properties property;
-	private MimeMessage msg;
-	Session session;
-	
-	private MailLib() {}
-	
-	private static class MailLibSingleton{
-		private static final MailLib instance = new MailLib();
-	}
-	
-	// 초기화 블록을 사용하여 최초 한번만 Default Setting을 부여한다.
-	{
-		property = new Properties();
-		try {
-		    
-		    InputStream inputStream = getClass().getClassLoader().getResourceAsStream("googleProperties/googleAccout.properties");
-		    
-		    property.load(inputStream);
-		    
-		    //람다가 안됨; https://stackoverflow.com/questions/26875226/using-a-lambda-expression-in-session
-		    session = Session.getDefaultInstance(property, new Authenticator(){
-		    	public PasswordAuthentication getPasswordAuthentication() {
-		    		return new PasswordAuthentication(property.getProperty("mail.smtp.username"), property.getProperty("mail.smtp.password"));
-		    	}
-		    });
-		    
-			System.out.println("로드 완료");
-		} catch (IOException e) {
-			System.out.println("로드 실패");
-			e.printStackTrace();
-		}
-		
-	} 
-	
-	public static MailLib getInstance() {
-		return MailLibSingleton.instance;
-	}
-	
-	
-	public boolean basicTextSendEmail(String title, String Content){
-		boolean bool = false;
-		
-		/*mailSender.setJavaMailProperties(property);
-		msg = mailSender.createMimeMessage();
-
-		try {
-			msg.setSubject(title);
-			msg.setContent("본문 내용", "utf-8");
-			msg.setText(Content);
-			msg.setFrom(new InternetAddress("tony950620@gmail.com"));
-			msg.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse("tony950620@naver.com"));
-			
-			mailSender.send(msg);
-			
-			bool = true;
-			
-		}catch(MessagingException err) {
-			System.out.println("에러 : " + err);
-		}*/
-		
-		return bool;
-	}
-	
-	public void sendTest() throws UnsupportedEncodingException{
-		
-		try {
-			msg = new MimeMessage(session);
-			
-			msg.setFrom(new InternetAddress("tony950620@gmail.com"));
-			msg.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse("tony950620@naver.com"));
-			
-			String htmlContent = "<strong>안녕하세요</strong>, 반갑습니다." + "<img src=" + "http://mwav.net/Images/CompanyLogos/CompanyLogo.gif" + ">";
-			msg.setSubject("Mwav Test Mail", "UTF-8");
-			
-			MimeBodyPart mbp1 = new MimeBodyPart();
-			File file = new File("D:\\unnamed.png");
-//			FileDataSource fds = new FileDataSource(file.getAbsolutePath());
-//			
-//			mbp1.setDataHandler(new DataHandler(fds));
-//			mbp1.setFileName(fds.getName());
-			mbp1.attachFile(file);
-			
-			
-			MimeBodyPart mbp2 = new MimeBodyPart();
-			mbp2.setContent(htmlContent, "text/html; charset=utf-8");
-
-			VelocityConfig velocityConfig = new VelocityConfigurer();
-			Map<String, Object> emailMap = new HashMap<String, Object>(); 
-			emailMap.put("PW", "값");
-			String veloTemplate = VelocityEngineUtils.mergeTemplateIntoString(velocityConfig.getVelocityEngine(), "GeneralMail/PWSeekEmail.vm", "UTF-8", emailMap);
-			System.out.println("벨로시티 : " + veloTemplate);
-			Multipart mp = new MimeMultipart();
-			
-			mp.addBodyPart(mbp1);
-			mp.addBodyPart(mbp2);
-			
-			//msg.setContent(mp);
-			msg.setText(veloTemplate);
-			
-			//Transport.send(msg);
-		} catch (MessagingException e) {
-			System.out.println("에러남 : " + e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-}
-
-=======
 package net.mwav.common.module;
 
 import java.io.File;
@@ -180,6 +16,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.apache.velocity.exception.VelocityException;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -217,7 +54,7 @@ public class MailLib {
 	private VelocityConfigurer velocityConfig = new VelocityConfigurer();
 	
 	private String fromAddress;
-	private String fromAddressName = "";
+	private String fromAddressName = "thKong";
 	private String host;
 	private String post;
 	
@@ -225,7 +62,8 @@ public class MailLib {
 	private final String contentEncoding = "text/html; charset=UTF-8";
 	
 	private MailLib() {
-		velocityConfig.setResourceLoaderPath("Templates/");
+		//velocityConfig.setResourceLoaderPath("Templates/");
+		velocityConfig.setResourceLoaderPath("/Templates");
 	}
 	
 	private static class MailLibSingleton{
@@ -286,63 +124,26 @@ public class MailLib {
 		return VelocityEngineUtils.mergeTemplateIntoString(velocityConfig.createVelocityEngine(), templatePath, encoding, map);
 	}
 	
-	// MimeMessage객체로 이메일 발송
-	private boolean sendEmail(MimeMessage msg){
-		boolean check = false;
-		try{
-			javaMailSender.send(msg);
-			check = true;
-		}catch(MailException msgErr){
-			System.out.println("이메일 발송 에러 - " + msgErr.getMessage());
-			msgErr.printStackTrace();
-		}
-		return check;
-	}
-	
 	// MimeMessageHelper객체로 이메일 발송
-	private boolean sendEmail(MimeMessageHelper msg){
-		boolean check = false;
+	private void sendEmail(MimeMessageHelper msg){
 		try{
 			javaMailSender.send(msg.getMimeMessage());
-			check = true;
 		}catch(MailException msgErr){
 			System.out.println("이메일 발송 에러 - " + msgErr.getMessage());
 			msgErr.printStackTrace();
 		}
-		return check;
 	}
 	
-	public void sendEmailAction(String Subject, String HtmlContents, File file, String toAddress){
-
-		try {
-			MimeMessage message = createMimeMessage();
-			
-			MimeMessageHelper msg = new MimeMessageHelper(message, true, encoding);
-				
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("TempPw", "테스트입니다.");
-			
-			String veloTemplate = getVeloTemplate(map, "GeneralMail/PWSeekEmail.vm");
-
-			msg.setSubject(Subject);
-			msg.setTo(toAddress);
-			msg.setFrom(fromAddress);
-			msg.setText(veloTemplate, true);
-			
-			javaMailSender.send(message);
-			
-		} catch (MessagingException e) {
-			System.out.println("MessagingException - ERROR " + e.getMessage());
-		} catch (VelocityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	//이메일 발신을 위한 최소한의 셋팅, 모든 발신 메소드들은 이 메소드를 필수로 실행해야한다.
+	private void emailRequiredSet(MimeMessageHelper msg, String toAddress, 
+			String subject, String templatePath, Map map) throws MessagingException, VelocityException, IOException {
+		msg.setFrom(fromAddress, fromAddressName);
+		msg.setSubject(subject);
+		msg.setTo(toAddress);
+		msg.setText(getVeloTemplate(map, templatePath), true);
 	}
 	
+	//임시 비밀번호 발급
 	public void sendCreatePassword(CommandMap commandMap) throws Exception {
 		
 		String mbrEmail = (String) commandMap.get("mbrEmail");
@@ -351,19 +152,52 @@ public class MailLib {
 		
 		MimeMessageHelper msg = new MimeMessageHelper(createMimeMessage(), true, encoding);
 		
-		msg.setSubject("Mwav 홈페이지 " + mbrLoginId + "님 [임시비밀번호]발급 메일입니다.");
-
-		msg.setFrom(fromAddress);
-		msg.setTo(mbrEmail);
-		
+		//password set
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("TempPw", mbrTempLoginPw);
-		msg.setText(getVeloTemplate(map, "GeneralMail/PWSeekEmail.vm"), true);
+		
+		//email require setting
+		emailRequiredSet(msg
+					   , mbrEmail
+					   , "Mwav 홈페이지 " + mbrLoginId + "님 [임시비밀번호]발급 메일입니다."
+					   , "/GeneralMail/PWSeekEmail.vm"
+					   , map);
+		
+		//send email
+		sendEmail(msg);
+	}
+	
+	// 멤버 회원가입시 이메일 발송
+	public void sendRegistrationEmail(Map<String, Object> map) throws Exception {
+		
+		MimeMessageHelper msg = new MimeMessageHelper(createMimeMessage(), true, encoding);
+		
+		//email require setting		
+		emailRequiredSet(msg
+				      , (String) map.get("mbrEmail")
+				      , "[Mwav]" + (String) map.get("mbrLoginId") + "님, 회원가입을 환영합니다."
+				      , "/GeneralMail/GeneralMail_Registration.vm"
+				      , map);
 		
 		sendEmail(msg);
+		//emailTemplates.sendAlertEmail("회원이 가입되었습니다."); // 관리자 Alert
+	}
+	
+	// 질문 메일 발신?
+	public void sendQuestionEmail(Map<String, Object> map) throws Exception {
+		
+		MimeMessageHelper msg = new MimeMessageHelper(createMimeMessage(), true, encoding);
+		
+		emailRequiredSet(msg
+				       , (String) map.get("uqUserEmail")
+				       , "[접수]_"+(String) map.get("uqUserName") + "님 문의해주신 내용이 정상 접수 되었습니다."
+				       , "QnAnswer/Question.vm"
+				       , map); 
+		
+		sendEmail(msg);
+//		emailTemplates.sendAlertEmail("문의가 접수되었습니다."); // 관리자 Alert
 	}
 	
 	
 }
 
->>>>>>> 7f9d6f6bad16e686ef5a48e268d33c4d5cbd8000
