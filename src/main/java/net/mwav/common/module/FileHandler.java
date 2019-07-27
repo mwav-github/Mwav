0cb2ad5559286f01fe549841448a8f3f9aaaa5b2
jungjh <jungjh@sogang.ac.kr>
2019-07-27 17:28:14 +0900

    FileHandler 정리

--- src/main/java/net/mwav/common/module/FileHandler.java
+++ src/main/java/net/mwav/common/module/FileHandler.java
@@ -2,222 +2,138 @@ package net.mwav.common.module;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 /**
- * HandlerFile
- * @author 정재현
- * @version 2019.06.01 v1.0
+ * @class name : FileHandler.java
+ * @description : FileUtils를 위한 branch project
+ * @author : (정) 정재현 (부)
+ * @since : 2019. 7. 24.
+ * @version : v1.0
+ * @see 
+ *  #method : File 업로드,다운,삭제 처리
+ * @history : 
+ * ---------------------------------------- 
+ * Modification Information(개정이력) 
+ * ---------------------------------------- 
+ * 수정일             수정자         수정내용 
+ * -------- -------- ---------------- 
+ * 2019.      7. 24. 정재현 최초작성
  */
+@Component("fileHandler")
 public class FileHandler {
-    private String filePath; // common
-    private MultipartHttpServletRequest multipartRequest; // upload
-    private Map<String, List<String>> fileNames; // upload
-    private String oldName; // down
-    private HttpServletResponse response; // down
-    private byte[] fileByte; // down
-    public FileHandler() {}
-    
-    /**
-     * common filePath
-     * @return filePath
-     */
-    public String getFilePath(){
-        return filePath;
-    }
-    /**
-     * upload 생성자
-     * @param multipartRequest
-     * @param filePath
-     */
-    public FileHandler(MultipartHttpServletRequest multipartRequest, String filePath) {
-        this.multipartRequest = multipartRequest;
-        this.filePath = filePath;
-        fileNames = new HashMap<String, List<String>>();
-    }
-    /**
-     * upload 임시저장 처리 생성자
-     * @param multipartRequest
-     */
-    public FileHandler(MultipartHttpServletRequest multipartRequest) {
-        this.multipartRequest = multipartRequest;
-        this.filePath = multipartRequest.getSession().getServletContext().getRealPath("/resources/tempfile");
-        fileNames = new HashMap<String, List<String>>();
-    }
-    /**
-     * upload setter
-     * @param multipartRequest
-     * @param filePath
-     * @return this
-     */
-    public FileHandler setUpload(MultipartHttpServletRequest multipartRequest, String filePath) {
-        this.multipartRequest = multipartRequest;
-        this.filePath = filePath;
-        fileNames = new HashMap<String, List<String>>();
-        return this;
-    }
-    /**
-     * upload 임시저장 처리 setter
-     * @param multipartRequest
-     * @return this
-     */
-    public FileHandler setUpload(MultipartHttpServletRequest multipartRequest) {
-        this.multipartRequest = multipartRequest;
-        this.filePath = multipartRequest.getSession().getServletContext().getRealPath("/resources/tempfile");
-        fileNames = new HashMap<String, List<String>>();
-        return this;
-    }
-    /**
-     * upload
-     * @return Map<String, List<String>> fileNames
-     */
-    public Map<String, List<String>> getUploadFileName() {
-        upload(true);
-        return fileNames;
-    }
-    /**
-     * upload save이름 생성여부
-     * @param saveNameCreate
-     * @return Map<String, List<String>> fileNames
-     */
-    public Map<String, List<String>> getUploadFileName(boolean saveNameCreate) {
-        //upload(saveNameCreate);
-        return fileNames;
-    }
-    /**
-     * 파일 업로드 처리
-     * @param saveNameCreate
-     */
-    public void upload(boolean saveNameCreate) {
-        Iterator<String> itr = multipartRequest.getFileNames();
-        List<String> oldNames = new ArrayList<String>();
-        List<String> saveNames = new ArrayList<String>();
-        StringBuffer sb = null;
-        while (itr.hasNext()) { // 받은 파일들을 모두 돌린다.
-            MultipartFile mpf = multipartRequest.getFile(itr.next());
-            sb = new StringBuffer();
-            String oldFileName = mpf.getOriginalFilename(); // 파일명
-            String saveFileName = sb.append(new SimpleDateFormat("yyyyMMdd").format(System.currentTimeMillis()))
-                                    .append("-")
-                                    .append(UUID.randomUUID().toString())
-                                    .append(oldFileName.substring(oldFileName.lastIndexOf("."))).toString();
-            String fileFullPath = null; // 파일 전체 경로
-            if(saveNameCreate) {
-                fileFullPath = filePath + "/" + saveFileName; 
-            }else {
-                fileFullPath = filePath + "/" + oldFileName;
-            }
-            try {
-                // 경로가 없으면 생성
-                File targetDir = new File(fileFullPath);
-                if(!targetDir.exists()) {
-                    targetDir.mkdirs();
-                }
-                // 파일 저장
-                mpf.transferTo(new File(fileFullPath));
-                oldNames.add(oldFileName);
-                if(saveNameCreate) {
-                    saveNames.add(saveFileName);
-                }else {
-                    saveNames.add(oldFileName);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        fileNames.put("oldNames", oldNames);
-        fileNames.put("saveNames", saveNames);
-    }
-    
-    /**
-     * down 생성자
-     * @param response
-     * @param filePath
-     * @param saveName
-     * @param oldName
-     */
-    public FileHandler(HttpServletResponse response, String filePath, String saveName, String oldName) {
-        this.response = response;
-        this.filePath = filePath + "/" + saveName;
-        this.oldName = oldName;
-    }
-    /**
-     * down setter
-     * @param response
-     * @param filePath
-     * @param saveName
-     * @param oldName
-     * @return this
-     */
-    public FileHandler setDown(HttpServletResponse response, String filePath, String saveName, String oldName) {
-        this.response = response;
-        this.filePath = filePath + "/" + saveName;
-        this.oldName = oldName;
-        return this;
-    }
-    /**
-     * down
-     * @return byte[] fileByte
-     */
-    public byte[] getDownloadFileByte() {
-        dowonload();
-        return fileByte;
-    }
-    /**
-     * down
-     */
-    private void dowonload() {
-        try {
-            fileByte = FileUtils.readFileToByteArray(new File(filePath));
-            response.setContentType("application/octet-stream");
-            response.setContentLength(fileByte.length);
-            // 다운로드시 변경할 파일명
-            response.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(oldName, "UTF-8") + "\";");
-            response.setHeader("Content-Transfer-Encoding", "binary");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    /**
-     * delete 생성자
-     * @param filePath
-     * @param saveName
-     */
-    public FileHandler(String filePath, String saveName) {
-        this.filePath = filePath + "/" + saveName;
-    }
-    /**
-     * delete setter
-     * @param filePath
-     * @param saveName
-     * @return this
-     */
-    public FileHandler setDelete(String filePath, String saveName) {
-        this.filePath = filePath + "/" + saveName;
-        return this;
-    }
-    /**
-     * delete
-     * @return boolean check
-     */
-    public boolean deleteFileExecute() {
-        boolean check = false;
-        File file = new File(filePath);
-        if(file.exists()) {
-            check = file.delete();
-        }
-        return check;
-    }
-    
+	// private List<String> fileNames; // upload
+	// private byte[] fileByte; // down
+
+	/**
+	 * @method name : FileHandler
+	 * @author : (정) 정재현
+	 * @since : 2019. 7. 24.
+	 * @version : v1.0
+	 * @see : #method : upload
+	 * @description : multipartRequest 파일업로드(저장)
+	 * @history : 
+	 * ---------------------------------------- 
+	 * Modification Information(개정이력) 
+	 * ---------------------------------------- 
+	 * 수정일  		    수정자	    수정내용 
+	 * -------- -------- ---------------- 
+	 * 2019. 7.24. 정재현 		최초작성
+	 * @param : MultipartHttpServletRequest multipartRequest
+	 * @param : String filePath	 
+	 * @return : List<String>: Multi 파일업로드 고려
+	 * @throws :
+	 * 
+	 * <pre>
+	 * {@code : 예제 코드 작성
+	 *  List<String> fileNames = fileHandler.upload(multipartRequest, filePath);
+	 * }
+	 * </pre>
+	 */
+	public List<String> upload(MultipartHttpServletRequest multipartRequest, String filePath) {
+		List<String> fileNames = new ArrayList<String>();
+
+		// 받은 파일들을 모두 돌린다.
+		Iterator<String> itr = multipartRequest.getFileNames();
+		while (itr.hasNext()) {
+			MultipartFile mpf = multipartRequest.getFile(itr.next());
+			String originFileName = mpf.getOriginalFilename(); // 파일명
+			String fileFullPath = null; // 파일 전체 경로
+			fileFullPath = filePath + "/" + originFileName;
+
+			try {
+				// 경로가 없으면 생성
+				File targetDir = new File(fileFullPath);
+				if (!targetDir.exists()) {
+					targetDir.mkdirs();
+				}
+				// 파일 저장
+				mpf.transferTo(new File(fileFullPath));
+				fileNames.add(originFileName);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return fileNames;
+	}
+
+	/**
+	 * @method name : FileHandler
+	 * @author : (정) 정재현
+	 * @since : 2019. 7. 24.
+	 * @version : v1.0
+	 * @see : #method : deleteFile
+	 * @description : 저장경로 파일삭제
+	 * @history : 
+	 * ---------------------------------------- 
+	 * Modification Information(개정이력) 
+	 * ---------------------------------------- 
+	 * 수정일  		    수정자	    수정내용 
+	 * -------- -------- ---------------- 
+	 * 2019. 7.24. 정재현 		최초작성
+	 * @param : String filePath	 
+	 * @param : String fileName	 
+	 * @return : boolean success flag return: 성공하면 true return
+	 * @throws :
+	 * 
+	 * <pre>
+	 * {@code : 예제 코드 작성
+	 *  boolean success = fileHandler.deleteFile(filePath, "savedFileName.jpg");
+	 * }
+	 * </pre>
+	 */
+	public boolean deleteFile(String filePath, String fileName) {
+		boolean success = false;
+		String fullPath = filePath + "/" + fileName;
+		File file = new File(fullPath);
+		if (file.exists()) {
+			success = file.delete();
+		}
+		return success;
+	}	
+	
+	public byte[] dowonload(HttpServletResponse response, String filePath, String fileName) {
+		
+		String fullPath = filePath + "/" + fileName;
+		// byte[] fileByte = FileUtils.readFileToByteArray(new File(fullPath));
+		byte[] fileByte = null;
+		try {
+			fileByte = FileUtils.readFileToByteArray(new File(fullPath));
+			response.setContentType("application/octet-stream");
+			response.setContentLength(fileByte.length);			
+			response.setHeader("Content-Transfer-Encoding", "binary");			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		return fileByte;
+	}
 }
