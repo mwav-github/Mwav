9cfce3fb901a87cbc2fc060b64eb2251f7c1d285
tony950620 <tony950620@gmail.com>
2019-08-31 10:14:18 +0900

    이메일 빌더 커밋

--- src/main/java/net/mwav/common/module/MessageBuilder.java
+++ src/main/java/net/mwav/common/module/MessageBuilder.java
@@ -2,7 +2,6 @@ package net.mwav.common.module;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import javax.mail.Address;
@@ -125,25 +124,124 @@ public class MessageBuilder{
       return (Address[]) addressList.toArray();
    }
    
-   public Address[] convertAddress(Map<String, String> addressList) throws AddressException {
-	  return null; 	
-   }
-   
+   /** 
+	 * <pre>description : 이메일을 제목을 설정한다.</pre>
+	 * @method name : setSubject
+	 * @author : (정) 공태현
+	             (부)
+	 * @since  : 2019. 8. 31.
+	 * @version : v1.0
+	 * @see : 
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+		  수정일 	          수정자    		        수정내용
+	   --------    --------    ----------------
+	   2019. 8. 31.     John     
+	 * @param : 이메일 제목 문자열
+	 * @return : 자기자신을 반환받아 builder 패턴을 적용
+	 * @throws : MessagingException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * setSubject("이메일 제목입니다.");
+	 * } 
+	 </pre>
+	*/
    public MessageBuilder setSubject(String subject) throws MessagingException {
       this.msg.setSubject(subject);
       return this;
    }
    
-   public MessageBuilder setText(String text) throws MessagingException {
-      this.msg.setText(text);
+   /** 
+	 * <pre>description : 이메일 본문을 작성한다. htmlYN이 true라면 html형식으로 false면 text로 적용</pre>
+	 * @method name : setContent
+	 * @author : (정) 공태현
+	             (부)
+	 * @since  : 2019. 8. 31.
+	 * @version : v1.0
+	 * @see :
+	   #method : 현재 클래스의 메소드나 변수 연결
+	   MyClass#method : MyClass 클래스의 메소드나 변수 연결
+	   my.package.MyClass#method : my.package 에 있는 MyClass 클래스의 메소드나 변수 연결
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+		  수정일 	          수정자    		        수정내용
+	   --------    --------    ----------------
+	   2019. 8. 31.     John     
+	 * @param : String타입의 본문, html적용여부 Y/N
+	 * @return : 자기자신을 반환받아 builder 패턴을 적용
+	 * @throws : MessagingException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * String content = VelocityEngineUtils.mergeTemplateIntoString(velocityConfig.createVelocityEngine(), "/Templates/GeneralMail/PWSeekEmail.vm", "UTF-8", map);
+	 * setContent(content, true)
+	 * } 
+	 </pre>
+	*/
+   public MessageBuilder setContent(String content, boolean htmlYN) throws MessagingException {
+	  if(htmlYN)
+		  this.msg.setContent(content, "text/html; charset=UTF-8");
+	  else
+		  this.msg.setText(content);
       return this;
    }
    
+   /** 
+	 * <pre>description : 수신자의 이메일주소를 받는다. 필수값</pre>
+	 * @method name : setRecipient
+	 * @author : (정) 공태현
+	             (부)
+	 * @since  : 2019. 8. 31.
+	 * @version : v1.0
+	 * @see : javax.mail.Message.RecipientType.TO
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+		  수정일 	          수정자    		        수정내용
+	   --------    --------    ----------------
+	   2019. 8. 31.     John     
+	 * @param : 전달받을 수신자 이메일주소
+	 * @return : 자기자신을 반환받아 builder 패턴을 적용
+	 * @throws : MessagingException, UnsupportedEncodingException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * setRecipent("email@gmail.com");
+	 * } 
+	 </pre>
+	*/
    public MessageBuilder setRecipient(String address) throws MessagingException, UnsupportedEncodingException {
       this.msg.setRecipient(RecipientType.TO, convertAddress(address));
       return this;
    }
    
+   /** 
+	 * <pre>description : 수신자의 이메일주소와 수신자 이름을 받는다. 필수값</pre>
+	 * @method name : setRecipient
+	 * @author : (정) 공태현
+	             (부)
+	 * @since  : 2019. 8. 31.
+	 * @version : v1.0
+	 * @see : javax.mail.Message.RecipientType.TO
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+		  수정일 	          수정자    		        수정내용
+	   --------    --------    ----------------
+	   2019. 8. 31.     John     
+	 * @param : 전달받을 수신자 이메일주소, 수신자 이름
+	 * @return : 자기자신을 반환받아 builder 패턴을 적용
+	 * @throws : MessagingException, UnsupportedEncodingException
+	 <pre>
+	 * {@code : 예제 코드 작성
+	 * DateUtil.addYearMonthDay("19810828", 0, 0, 19)  = "19810916"
+	 * } 
+	 </pre>
+	*/
    public MessageBuilder setRecipient(String address, String recipentName) throws MessagingException, UnsupportedEncodingException {
       Address ad = new InternetAddress(address, recipentName);
       this.msg.setRecipient(RecipientType.TO, ad);
