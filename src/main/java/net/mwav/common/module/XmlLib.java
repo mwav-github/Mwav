4c0b3561996cb2028383722cc87b7aef6667755f
gregor77kr <gregor77kr@gmail.com>
2019-09-18 13:20:34 +0900

    1. XML LIB 도입 시작

--- src/main/java/net/mwav/common/module/XmlLib.java
+++ src/main/java/net/mwav/common/module/XmlLib.java
@@ -1,5 +1,45 @@
 package net.mwav.common.module;
 
+import java.io.FileReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
 public class XmlLib {
+
+	private XmlLib() {
+	}
+
+	private static class XmlHolder {
+		private static final XmlLib xmlLib = new XmlLib();
+	}
+
+	public XmlLib getInstance() {
+		return XmlHolder.xmlLib;
+	}
+
+	/**
+	 * JAXBException, IOException
+	 * 
+	 * @param path
+	 * @param classObject
+	 * @return
+	 * @throws Exception
+	 */
+	public static Object unmarshal(final String path, final Class<?> classObject) throws Exception {
+
+		JAXBContext jaxbContext = null;
+		Unmarshaller unmarshaller = null;
+		Object unmarshallerObj = null;
+
+		// JAXB Context Create
+		jaxbContext = JAXBContext.newInstance(classObject);
+		// Unmarshaller Object Create
+		unmarshaller = jaxbContext.createUnmarshaller();
+		// unmarshall Create
+		unmarshallerObj = unmarshaller.unmarshal(new FileReader(path));
+		
+		return unmarshallerObj;
+	}
 
 }
