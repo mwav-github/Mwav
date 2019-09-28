f4de76d65baaa9b5b0a265e73c3e3e1956c95010
gregor77kr <gregor77kr@gmail.com>
2019-09-28 15:44:14 +0900

    xml lib 주석 처리 완료

--- src/main/java/net/mwav/common/module/XmlLib.java
+++ src/main/java/net/mwav/common/module/XmlLib.java
@@ -5,6 +5,23 @@ import java.io.FileReader;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
+/**
+ *<pre>description : Utility class to parse a XML document</pre>
+ *@class name : XmlLib.java
+ *@author : (정) 조도형
+            (부) 남동희
+ *@since : 2019. 9. 28.
+ *@version : v1.0
+ *@see
+   javax.xml.bind
+ * @history :
+   ----------------------------------------
+   * Modification Information(개정이력)
+   ----------------------------------------
+           수정일                   수정자                        수정내용
+   --------    --------    ----------------
+   2019. 9. 28.     78371   First commit
+ */
 public class XmlLib {
 
 	private XmlLib() {
@@ -13,18 +30,63 @@ public class XmlLib {
 	private static class XmlHolder {
 		private static final XmlLib xmlLib = new XmlLib();
 	}
-
+	
+	/**
+	 * <pre>description : Instance method of XmlLib Class</pre>
+	 * @method name : getInstance
+	 * @author : (정) 조도형
+	             (부) 남동희
+	 * @since  : 2019. 9. 28.
+	 * @version : v1.0
+	 * @see :
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	           수정일                  수정자                       수정내용
+	   --------    --------    ----------------
+	   2019. 9. 28.     78371     First commit
+	 * @param :
+	 * @return :
+	 * @throws : 
+	 <pre>
+	 * {@code :
+	 * ValidationLib xml = XmlLib.getInstance();
+	 * } 
+	 </pre>
+	 */
 	public static XmlLib getInstance() {
 		return XmlHolder.xmlLib;
 	}
 
 	/**
-	 * JAXBException, IOException
-	 * 
-	 * @param path
-	 * @param classObject
-	 * @return
-	 * @throws Exception
+	 * <pre>description : Parse a XML data to Java object</pre>
+	 * @method name : unmarshal
+	 * @author : (정) 조도형
+	             (부) 남동희
+	 * @since  : 2019. 9. 28.
+	 * @version : v1.0
+	 * @see :
+	   javax.xml.bind.JAXBContext
+	   javax.xml.bind.Unmarshaller
+	 * @history :
+	   ----------------------------------------
+	   * Modification Information(개정이력)
+	   ----------------------------------------
+	           수정일                  수정자                       수정내용
+	   --------    --------    ----------------
+	   2019. 9. 28.     78371     First commit
+	 * @param : 
+	   final String   path - Path of XML document
+	   final Class<?> cls  - Class to describe a XML tree structure(element and node)
+	 * @return : Java object which contains the XML data
+	 * @throws : Exception
+	 <pre>
+	 * {@code :
+	 * ValidationLib xml = XmlLib.getInstance();
+	 * Object xmlData = xml.unmarshal(path, xmlFormat);
+	 * } 
+	 </pre>
 	 */
 	public Object unmarshal(final String path, final Class<?> cls) throws Exception {
 
