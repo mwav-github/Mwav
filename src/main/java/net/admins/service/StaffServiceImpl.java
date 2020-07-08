54fe410db55054db089712b09b1191c62a17d893
tony <tony950620@naver.com>
2020-07-08 18:02:24 +0900

    담당직원 모달 페이징 오류 수정

--- src/main/java/net/admins/service/StaffServiceImpl.java
+++ src/main/java/net/admins/service/StaffServiceImpl.java
@@ -238,12 +238,13 @@ public class StaffServiceImpl implements StaffService {
 		resultMap.put("minCount", 0);
 
 		int maxCountPage = (int) Math.ceil(maxCount/10);
+
 		resultMap.put("maxCountPage", maxCountPage);
 
 		// 페이지의 최소값은 0보다 작을 수 없으며, 페이지가 maxCountPage, maxCountPage-1, maxCountPage-2 라면 maxCountPage 로 고정
 		int minPage = 0 > page-2 ? 0 : page-2;
 		if(minPage > maxCountPage-4 && maxCount != 0){
-			minPage = maxCountPage-4;
+			minPage = maxCountPage-4 < 0 ? 0 : maxCountPage-4;
 		}
 		resultMap.put("minPage", minPage);
 
