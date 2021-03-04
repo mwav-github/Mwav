abd2ebd9f51d346caed1c2920e8e7ffb7e18e985
tony <tony950620@naver.com>
2021-03-04 10:18:49 +0900

    테스트에서 사용할 파라미터를 @Before로 공통화

--- src/test/java/net/bizLogin/promoter/service/PmtFacilitatorServiceImplTest.java
+++ src/test/java/net/bizLogin/promoter/service/PmtFacilitatorServiceImplTest.java
@@ -3,6 +3,7 @@ package net.bizLogin.promoter.service;
 import net.bizLogin.promoter.dao.PmtFacilitatorDAO;
 import net.common.common.CommandMap;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -38,6 +39,34 @@ public class PmtFacilitatorServiceImplTest {
     @Mock
     PmtFacilitatorDAO dao;
 
+    // 테스트 멤버 파라미터
+    private CommandMap commandMap;
+
+    /**
+     * <pre>
+     * {@code
+     *      <p>각각의 테스트케이스마다 테스트에 사용할 멤버 파라미터를 생성</p>
+     * }
+     * </pre>
+     * @param
+     * @return void
+     * @throws
+     * @see
+     * @since 1.0.4
+     * @version 1.0.0
+     */
+    @Before
+    public void testParameterMember(){
+        this.commandMap = new CommandMap();
+        this.commandMap.put("pmtLoginId", "mwavtest");       // 아이디
+        this.commandMap.put("pmtLoginPw", "mwav123!!");       // 비밀번호
+        this.commandMap.put("pmtCellularPhone", "010-2866-2352"); // 핸드폰 번호
+        this.commandMap.put("pmtMail", "mwavtest@gmail.com");          // 이메일
+        this.commandMap.put("pmtGender", "M");        // 성별
+        this.commandMap.put("pmtFirstName", "공");     // 첫번째 이름
+        this.commandMap.put("pmtLastName", "태현");      // 이름
+    }
+
     /**
      * <pre>
      * {@code
@@ -54,15 +83,6 @@ public class PmtFacilitatorServiceImplTest {
     @Test
     public void insertPmtForm_DUPLICATED() throws Exception {
         // given
-        // 테스트 멤버 파라미터
-        CommandMap commandMap = new CommandMap();
-        commandMap.put("pmtLoginId", "mwavtest");       // 아이디
-        commandMap.put("pmtLoginPw", "mwav123!!");       // 비밀번호
-        commandMap.put("pmtCellularPhone", "010-2866-2352"); // 핸드폰 번호
-        commandMap.put("pmtMail", "mwavtest@gmail.com");          // 이메일
-        commandMap.put("pmtGender", "M");        // 성별
-        commandMap.put("pmtFirstName", "공");     // 첫번째 이름
-        commandMap.put("pmtLastName", "태현");      // 이름
 
         // when
         // 멤버 로그인시 중복된 아이디가 1개 존재한다.
@@ -74,7 +94,7 @@ public class PmtFacilitatorServiceImplTest {
         doNothing().when(dao).insertPromoterValueLog_tbl(any());
 
         // service를 실행하여 결과를 반환한다.
-        final Map<String, Object> map = service.insertPmtForm(commandMap);
+        final Map<String, Object> map = service.insertPmtForm(this.commandMap);
 
         // then
         // 반환된 결과 map의 키와 값이 containsOnly에 들어가야한다.
@@ -83,4 +103,42 @@ public class PmtFacilitatorServiceImplTest {
                 .containsOnly("31", "DUPLICATED");
     }
 
+    /**
+     * <pre>
+     * {@code
+     *      <p>멤버 회원가입시 유효하지않은 값이 넘겨왔을때 테스트</p>
+     * }
+     * </pre>
+     * @param
+     * @return void
+     * @throws IOException
+     * @see PmtFacilitatorService.insertPmtForm
+     * @since 1.0.4
+     * @version 1.0.0
+     */
+    @Test
+    public void insertPmtForm_INVALID() throws Exception {
+        // given
+
+        // when
+        // 입력받아온 pmtLoginId값이 한글이라 유효성조건에 일치하지않는 경우
+        commandMap.put("pmtLoginId", "엠웨이브");
+
+        // 멤버 로그인시 중복된 아이디는 없다.(정상)
+        when(dao.selectOnePmtLoginIdCheck(anyString())).thenReturn(0);
+
+        // 아이디 중복 테스트이기 때문에 insert 는 무시한다.
+        doNothing().when(dao).insertPromoter_tbl(any());
+        doNothing().when(dao).insertPromoterValue_tbl(any());
+        doNothing().when(dao).insertPromoterValueLog_tbl(any());
+
+        // service를 실행하여 결과를 반환한다.
+        final Map<String, Object> map = service.insertPmtForm(this.commandMap);
+
+        // then
+        // 반환된 결과 map의 키와 값이 containsOnly에 들어가야한다.
+        Assertions.assertThat(map)
+                .extracting("result", "message")
+                .containsOnly("42", "INVALID");
+    }
 }
