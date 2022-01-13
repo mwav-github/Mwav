1cfeb7feeb854a7cf8afcd378e206dd2521b6e91
gregor77kr <7837167@naver.com>
2022-01-13 23:34:53 +0900

    Update configuration
    - update net.mwav.framework to 1.0.8
    - revise logger

--- src/main/java/net/common/common/ErrorController.java
+++ src/main/java/net/common/common/ErrorController.java
@@ -1,83 +1,63 @@
 package net.common.common;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+/**
+ * 예외 페이지 처리
+ * TODO : @ControllerAdvice로 변경
+ *
+ */
 @Controller
 @RequestMapping("/MessageView")
 public class ErrorController {
-	Logger log = Logger.getLogger(this.getClass());
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
+
 	@RequestMapping(value = "/throwable.mwav")
-	public String throwable(Model model,
-			HttpServletRequest request) throws Exception {
-		
-		log.info("throwable");
+	public String throwable(Model model, HttpServletRequest request) throws Exception {
+		logger.debug("throwable");
 		pageErrorLog(request);
 		model.addAttribute("msg", "예외가 발생하였습니다.");
 
 		return "/MessageView/error";
 	}
-	
-	
-	
+
 	@RequestMapping(value = "/exception.mwav")
-	public String exception(Model model,
-			HttpServletRequest request) throws Exception {
-		
-		log.info("exception");
+	public String exception(Model model, HttpServletRequest request) throws Exception {
+		logger.debug("exception");
 		pageErrorLog(request);
 		model.addAttribute("msg", "예외가 발생하였습니다.");
 
 		return "/MessageView/error";
 	}
-	
+
 	@RequestMapping(value = "/400.mwav")
-	public String pageError400(Model model,
-			HttpServletRequest request) throws Exception {
-		
-		log.info("page error code 400");
+	public String pageError400(Model model, HttpServletRequest request) throws Exception {
+		logger.debug("page error code 400");
 		pageErrorLog(request);
 		model.addAttribute("msg", "잘못된 요청입니다.");
 
 		return "/MessageView/error";
 	}
-	
+
 	@RequestMapping(value = "/403.mwav")
-	public String pageError403(Model model,
-			HttpServletRequest request) throws Exception {
-		
-		log.info("page error code 403");
+	public String pageError403(Model model, HttpServletRequest request) throws Exception {
+		logger.debug("page error code 403");
 		pageErrorLog(request);
 		model.addAttribute("msg", "잘못된 요청입니다.");
 
 		return "/MessageView/error";
 	}
 
-	
 	@RequestMapping(value = "/404.mwav")
-	public String pageError404(Model model,
-			HttpServletRequest request) throws Exception {
-		System.out.println("고고");
-		log.info("page error code 404");
+	public String pageError404(Model model, HttpServletRequest request) throws Exception {
+		logger.debug("page error code 404");
 		pageErrorLog(request);
 		model.addAttribute("msg", "잘못된 요청입니다.");
 
@@ -85,47 +65,39 @@ public class ErrorController {
 	}
 
 	@RequestMapping(value = "/405.mwav")
-	public String pageError405(Model model,
-			HttpServletRequest request) throws Exception {
-		
-		log.info("page error code 405");
+	public String pageError405(Model model, HttpServletRequest request) throws Exception {
+		logger.debug("page error code 405");
 		pageErrorLog(request);
 		model.addAttribute("msg", "잘못된 요청입니다.");
 
 		return "/MessageView/error";
 	}
-	
+
 	@RequestMapping(value = "/500.mwav")
-	public String pageError500(Model model,
-			HttpServletRequest request) throws Exception {
-		
-		log.info("page error code 500");
+	public String pageError500(Model model, HttpServletRequest request) throws Exception {
+		logger.debug("page error code 500");
 		pageErrorLog(request);
 		model.addAttribute("msg", "잘못된 요청입니다.");
 
 		return "/MessageView/error";
 	}
-	
+
 	@RequestMapping(value = "/503.mwav")
-	public String pageError503(Model model,
-			HttpServletRequest request) throws Exception {
-		
-		log.info("page error code 503");
+	public String pageError503(Model model, HttpServletRequest request) throws Exception {
+		logger.debug("page error code 503");
 		pageErrorLog(request);
 		model.addAttribute("msg", "잘못된 요청입니다.");
 
 		return "/MessageView/error";
 	}
-	
-	private void pageErrorLog(HttpServletRequest request){
-		
-		
-		log.info("status_code : " +request.getAttribute("javax.servlet.error.status.code"));
-		log.info("exception_type : " +request.getAttribute("javax.servlet.error.exception_type"));
-		log.info("message : " +request.getAttribute("javax.servlet.error.message"));
-		log.info("request_uri : " +request.getAttribute("javax.servlet.error.request_uri"));
-		log.info("exception : " +request.getAttribute("javax.servlet.error.exception"));
-		log.info("servlet_name : " +request.getAttribute("javax.servlet.error.servlet_name"));
+
+	private void pageErrorLog(HttpServletRequest request) {
+		logger.debug("status_code : " + request.getAttribute("javax.servlet.error.status.code"));
+		logger.debug("exception_type : " + request.getAttribute("javax.servlet.error.exception_type"));
+		logger.debug("message : " + request.getAttribute("javax.servlet.error.message"));
+		logger.debug("request_uri : " + request.getAttribute("javax.servlet.error.request_uri"));
+		logger.debug("exception : " + request.getAttribute("javax.servlet.error.exception"));
+		logger.debug("servlet_name : " + request.getAttribute("javax.servlet.error.servlet_name"));
 	}
-	
+
 }
